c0cb9d27ff0d2ab45add4550ab539ae1d6b07c97
YangJL2003 <yangjl22@mails.tsinghua.edu.cn>
2025-01-13 17:04:27 +0800

    Clean code

--- training_data/component_render/react-app/src/App.js
+++ training_data/component_render/react-app/src/App.js
@@ -5,7 +5,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import CheckboxComponent from './CheckboxComponent';
 
 function App() {
   return (
